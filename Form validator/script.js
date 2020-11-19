bbec96a4bff23d6d91ceda945f77a3177252760a
Andrea Rus <andrearus27@gmail.com>
2020-11-19 15:54:31 +0000

    event listener

--- Form validator/script.js
+++ Form validator/script.js
@@ -1,13 +1,19 @@
+const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
-
-const userInput = username.value;
-
-console.log(userInput);
-
-username.addEventListener('onload', e => {
-
+form.addEventListener('submit', function(e) {
   e.preventDefault();
-  userInput;
-})
+
+  const userInput = username.value;
+
+  if(userInput === '') {
+    html += this.innerHTML
+    console.log()
+  }
+});
+
+
+
