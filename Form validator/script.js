5104275b23650e1ee35612c2a16c2f6985456e67
Andrea Rus <andrearus27@gmail.com>
2020-11-27 10:37:00 +0000

    Update script.js

--- Form validator/script.js
+++ Form validator/script.js
@@ -4,36 +4,39 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
+// Show input error message
 function showError(input, message) {
-
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
-  const small  = formControl.querySelector('small');
+  const small = formControl.querySelector('small');
   small.innerText = message;
 }
 
+// Show success outline
 function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = 'form-control success';
 }
 
-function isValidEmail() {
+// Check email is valid
+function checkEmail(input) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
 }
 
+// Check required fields
 function checkRequired(inputArr) {
-  inputArr.forEach(function(input) { 
-    if(input.value.trim() === '') {
+  inputArr.forEach(function(input) {
+    if (input.value.trim() === '') {
       showError(input, `${getFieldName(input)} is required`);
     } else {
       showSuccess(input);
     }
   });
-}
-
-function getFieldName(input) {
-  return input.id.charAt(0).toUpperCase + input.id.slice(1);
 }
 
 
